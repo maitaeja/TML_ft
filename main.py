ba59c9a418e16f954712933ce4d19ba705739912
maitaeja <maita.eja@gmail.com>
2026-08-29 13:51:00 -0500

    Enable web browser view for the app

--- main.py
+++ main.py
@@ -25,9 +25,9 @@ def main(page: ft.Page):
 if __name__ =="__main__":
     port = int(os.environ.get("PORT", 8080))
     ft.app(target=main,
-           #view = ft.AppView.WEB_BROWSER,
-           #host = "0.0.0.0",
-           #port = port
+           view = ft.AppView.WEB_BROWSER,
+           host = "0.0.0.0",
+           port = port
            )
 
 
