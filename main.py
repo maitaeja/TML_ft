4423c03ae558529e5f0cf00ae7a46548856edf8a
maita.eja <maita.eja@gmail.com>
2026-08-29 16:52:59 -0500

    fix render app web layout

--- main.py
+++ main.py
@@ -22,13 +22,14 @@ def main(page: ft.Page):
     page.add(app)
     page.update()
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    ft.app(target=main,
-           view = ft.AppView.WEB_BROWSER,
-           host = "0.0.0.0",
-           port = port
-           )
+    ft.app(
+        target=main,
+        view=ft.AppView.WEB_BROWSER,
+        host="0.0.0.0",
+        port=port
+    )
 
 
 
