17fca9fd472d1b0f537fcc06ee8864faa85a57da
maita.eja <maita.eja@gmail.com>
2026-08-30 01:50:49 -0500

    configurar assets_dir para cargar imagen en la web

--- main.py
+++ main.py
@@ -26,7 +26,8 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     ft.app(
         target=main,
-        # view=ft.AppView.WEB_BROWSER,
+        assets_dir="imagenes",
+        view=ft.AppView.WEB_BROWSER,
         host="0.0.0.0",
         port=port
         )
